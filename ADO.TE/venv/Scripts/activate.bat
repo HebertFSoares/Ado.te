88c318fc72abeaf69d0243083526571bcccf6f1d
Hebert Soares <hebertsoaresof@gmail.com>
2023-08-17 15:56:13 -0300

    Adicionando Configurações de deploy

--- ADO.TE/venv/Scripts/activate.bat
+++ ADO.TE/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\heber\Desktop\Ado.te-DJANGO\ADO.TE\venv
+set VIRTUAL_ENV=C:\Users\heber\Documents\GitHub\PROJETOS(Python)\Ado.te-DJANGO\ADO.TE\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
